75dc42513dc4a770bc328090b645998eab2be0ba
vashi38 <shivanandsonnad@gmail.com>
2018-06-10 02:27:51 +0530

    final commit

--- conf/webpack-test.conf.js
+++ conf/webpack-test.conf.js
@@ -9,6 +9,16 @@ module.exports = {
         ]
       },
       {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loaders: [
